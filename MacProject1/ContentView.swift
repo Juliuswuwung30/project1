7f0d29e4f5967a8bbb2ff4b32084bb6258935b93
TheJukius <127324193+Juliuswuwung30@users.noreply.github.com>
2024-05-02 12:34:36 +0700

    Add files via upload

---
+++ MacProject1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacProject1
+//
+//  Created by student on 26/04/24.
+//
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
